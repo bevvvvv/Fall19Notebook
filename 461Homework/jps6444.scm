e2c3c4632364c51e07aa65169b30f6255716cbde
Joseph Sepich <jpsepich@andrew.cmu.edu>
2019-09-22 16:38:03 -0400

    Started problem 2

--- 461Homework/jps6444.scm
+++ 461Homework/jps6444.scm
@@ -5,6 +5,12 @@
 (define (LEFT p) (p (lambda (t f) t)))
 (define (RIGHT p) (p (lambda (t f) f)))
 
+; Problem 2
+; inductive function composition
+(define (evaluate inner outer) (eval '(outer inner))); define composition
+
+(define (funcompose lst) (lambda (x) (fold-left evaluate x lst))); fold list into composition
+
 
 
 
