31ad838a1e2978d2c17adc4afcffe29ce459e9f1
Joseph Sepich <computerwhiz1337@gmail.com>
2019-09-23 14:06:34 -0400

    problem 3 done

--- 461Homework/jps6444.scm
+++ 461Homework/jps6444.scm
@@ -11,6 +11,17 @@
 
 (define (funcompose lst) (lambda (x) (fold-right evaluate x lst))); fold list into composition
 
+; Problme 3
+; merge from merge sort
+(define (merge l r)
+	(if (null? l) r
+		; l is not null		
+		(if (null? r) l
+			; neither are null
+			(if (> (car l) (car r))
+				(cons (car r) (merge l (cdr r)))
+				(cons (car l) (merge (cdr l) r))))))
+
 
 
 
